2cff2d357ae16f0ac2f101ba0ab56cb4f2e4f1e1
Artem <artem.ysanov2217@gmail.com>
2022-12-25 23:20:51 +0300

    text

---
+++ src/com/company/SqClient.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class SqClient {
+
+    public static void main(String[] args) throws IOException {
+        try (Socket socket = new Socket("localhost", 1020)) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Введите фразу: ");
+            String messages = sc.nextLine();
+            DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
+            outputStream.writeUTF(messages);
+            DataInputStream inputStream = new DataInputStream(socket.getInputStream());
+            messages = inputStream.readUTF();
+            System.out.println("Результат:" + messages);
+            outputStream.writeUTF(messages);
+        }
+    }
+}
